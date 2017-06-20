171f97f22928f7299f886611b7dae85cefd316dd
Brent Busby <brent.busby@yahoo.com>
2017-06-20 05:51:00 -0500

    Complete onboarding form

--- app/controllers/submerchants_controller.rb
+++ app/controllers/submerchants_controller.rb
@@ -1,16 +1,55 @@
 class SubmerchantsController < ApplicationController
 
   def new
-
-    submerchant = {
-      :id => params[:id],
-      :first_name => params[:first_name]
-    }
-
-
-    result = Braintree::MerchantAccount.create(merchant_account_params)
-
   end
 
+  def create
 
+    merchant_account_params = {
+      :individual => {
+        :first_name => params[:merchant_account_params][:first_name],
+        :last_name => params[:merchant_account_params][:last_name],
+        :email => params[:merchant_account_params][:email],
+        :phone => params[:merchant_account_params][:phone],
+        :date_of_birth => params[:merchant_account_params][:date_of_birth],
+        :ssn => params[:merchant_account_params][:ssn],
+        :address => {
+          :street_address => params[:merchant_account_params][:street_address],
+          :locality => params[:merchant_account_params][:locality],
+          :region => params[:merchant_account_params][:region],
+          :postal_code => params[:merchant_account_params][:postal_code]
+        }
+      },
+      :business => {
+        :legal_name => params[:merchant_account_params][:legal_name],
+        :dba_name => params[:merchant_account_params][:dba_name],
+        :tax_id => params[:merchant_account_params][:tax_id],
+        :address => {
+          :street_address => params[:merchant_account_params][:business_street],
+          :locality => params[:merchant_account_params][:business_locality],
+          :region => params[:merchant_account_params][:business_region],
+          :postal_code => params[:merchant_account_params][:business_postal_code]
+        }
+      },
+      :funding => {
+        :descriptor => params[:merchant_account_params][:descriptor],
+        :destination => Braintree::MerchantAccount::FundingDestination::Bank,
+        :account_number => params[:merchant_account_params][:account_number],
+        :routing_number => params[:merchant_account_params][:routing_number]
+      },
+      :id => params[:merchant_account_params][:id],
+      :master_merchant_account_id => "INSERT MERCH ACCOUNT HERE!!!!!!",
+      :tos_accepted => params[:merchant_account_params][:tos]
+    }
+    puts params
+    puts merchant_account_params
+    result = Braintree::MerchantAccount.create(merchant_account_params)
+    puts result
+    result.errors.each do |error|
+      puts error.attribute
+      puts error.code
+      puts error.message
+    end
+
+  end
 end
