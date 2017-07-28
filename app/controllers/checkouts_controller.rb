e4f5857a080ab532b7c06c981c9a9823da49df85
Brent Busby <brent.busby@yahoo.com>
2017-07-28 05:58:58 -0500

    create working build of checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,6 +1,24 @@
 class CheckoutsController < ApplicationController
 
   def new
+    @client_token = Braintree::ClientToken.generate
+  end
+
+  def create
+    amount = "10.00"
+     nonce = params["payment_method_nonce"]
+
+     result = Braintree::Transaction.sale(
+       amount: amount,
+       payment_method_nonce: nonce,
+       :options => {
+         :submit_for_settlement => true
+       }
+     )
+
+     puts result
+     puts result.params
+     puts result.errors
 
   end
 
